2085b7c2ec4900c0b60b22df7ebf8dbc803bf9b3
Rodrigo Tassini <rodrigo.tassini@gmail.com>
2025-06-03 11:16:00 -0300

    FEAT project created

---
+++ src/main/java/com/recargapay/walletservice/WalletserviceApplication.java
@@ -0,0 +1,13 @@
+package com.recargapay.walletservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletserviceApplication.class, args);
+	}
+
+}
